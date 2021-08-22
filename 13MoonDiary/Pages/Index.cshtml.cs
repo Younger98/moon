5de2ea11a8dad1ebde782ffbc905cbcad8b5133b
Younger Yang <younger_yang@wistron.com>
2021-08-22 20:32:09 +0800

    Add week card info

--- 13MoonDiary/Pages/Index.cshtml.cs
+++ 13MoonDiary/Pages/Index.cshtml.cs
@@ -12,11 +12,20 @@ namespace _13MoonDiary.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        //以2021/01/01為基準, 當天為Friday, KIN18, 超頻(5)白世界橋(18)
+        private static DateTime baseDate = DateTime.Parse("Jan 1, 2021");
+        private static int baseKIN = 18;
+        private static int baseOrace = 18;
+        private static int baseTone = 5;
+
         [BindProperty]
         public static List<MayanOracle> MayanOracles { get; set; }
 
         [BindProperty]
-        public static List<Tone> tones { get; set; }
+        public static List<Tone> MayanTones { get; set; }
+
+        [BindProperty]
+        public static List<CardInfo> WeekCardInfos { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -26,10 +35,21 @@ namespace _13MoonDiary.Pages
         public void OnGet()
         {
             InitOracle();
-            var todayOracle = MayanOracles[5];
+            InitTone();
+
+            var todayOracle = GetMayanOracleByDate(DateTime.Now.Date);
+            var todayTone = GetToneByDate(DateTime.Now.Date);
+            string todayKIN = GetKINByDate(DateTime.Now.Date);
+
+            //從本週的第一天起取得連續七天的CardInfo
             DateTime dt = DateTime.Now.StartOfWeek(DayOfWeek.Monday);
+
+            WeekCardInfos = GetWeekCardInfos(dt);
         }
 
+        /// <summary>
+        /// 初始化波符料
+        /// </summary>
         public void InitOracle()
         {
             List<MayanOracle> oracles = new List<MayanOracle>
@@ -199,6 +219,10 @@ namespace _13MoonDiary.Pages
 
             MayanOracles = oracles;
         }
+
+        /// <summary>
+        /// 初始化調性資料
+        /// </summary>
         public void InitTone()
         {
             List<Tone> tones = new List<Tone>
@@ -208,9 +232,173 @@ namespace _13MoonDiary.Pages
                     id=1,
                     toneEName = "Magnetic",
                     toneName = "磁性",
-                    toneImgSrc = ""
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-magnetic.png"
+                },
+                new Tone
+                {
+                    id=2,
+                    toneEName = "Lunar",
+                    toneName = "月亮",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-lunar.png"
+                },
+                new Tone
+                {
+                    id=3,
+                    toneEName = "Electric",
+                    toneName = "電力",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-electric.png"
+                },
+                new Tone
+                {
+                    id=4,
+                    toneEName = "Self Existing",
+                    toneName = "自我存在",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-self-existing.png"
+                },
+                new Tone
+                {
+                    id=5,
+                    toneEName = "Overtone",
+                    toneName = "超頻",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-overtone.png"
+                },
+                new Tone
+                {
+                    id=6,
+                    toneEName = "Rhythmic",
+                    toneName = "韻律",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-rhythmic.png"
+                },
+                new Tone
+                {
+                    id=7,
+                    toneEName = "Resonant",
+                    toneName = "共振",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-resonant.png"
+                },
+                new Tone
+                {
+                    id=8,
+                    toneEName = "Galactic",
+                    toneName = "銀河星系",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-galactic.png"
+                },
+                new Tone
+                {
+                    id=9,
+                    toneEName = "Solar",
+                    toneName = "太陽",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-solar.png"
+                },
+                new Tone
+                {
+                    id=10,
+                    toneEName = "Planetary",
+                    toneName = "行星",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-planetary.png"
+                },
+                new Tone
+                {
+                    id=11,
+                    toneEName = "Spectral",
+                    toneName = "光譜",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-spectral.png"
+                },
+                new Tone
+                {
+                    id=12,
+                    toneEName = "Crystal",
+                    toneName = "水晶",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-crystal.png"
+                },
+                new Tone
+                {
+                    id=13,
+                    toneEName = "Cosmic",
+                    toneName = "宇宙",
+                    toneImgSrc = "mayankin-marielamaya-bridgingworlds-tzolkin-lunar-tone-cosmic.png"
                 }
             };
+
+            MayanTones = tones;
+        }
+
+        public MayanOracle GetMayanOracleByDate(DateTime dDate)
+        {
+            //波符為20天一個週期            
+            int dateDiff = Convert.ToInt32((dDate - baseDate).TotalDays);
+            int oracleIndex = dateDiff % 20;
+            if(oracleIndex > 2)
+            {
+                oracleIndex = oracleIndex - 2;
+            }
+            else
+            {
+                oracleIndex = oracleIndex + baseOrace;
+            }
+
+            return MayanOracles[oracleIndex-1];
+        }
+
+        public Tone GetToneByDate(DateTime dDate)
+        {
+            //調性為13天一個週期            
+            int dateDiff = Convert.ToInt32((dDate - baseDate).TotalDays);
+            int toneIndex = dateDiff % 13;
+            if (toneIndex > 8)
+            {
+                toneIndex = toneIndex - 8;
+            }
+            else
+            {
+                toneIndex = toneIndex + baseTone;
+            }
+
+            return MayanTones[toneIndex - 1];
+        }
+
+        public string GetKINByDate(DateTime dDate)
+        {
+            //KIN為260天一個週期
+            int dateDiff = Convert.ToInt32((dDate - baseDate).TotalDays);
+            int kinIndex = dateDiff % 260;
+            if (kinIndex > 242)
+            {
+                kinIndex = kinIndex - 242;
+            }
+            else
+            {
+                kinIndex = kinIndex + baseKIN;
+            }
+
+            return string.Format("KIN{0}", kinIndex);
+        }
+
+        public List<CardInfo> GetWeekCardInfos(DateTime weekStartDate)
+        {
+            List<CardInfo> cards = new List<CardInfo>();
+
+            for(int i=0; i<7; i++)
+            {
+                var dDate = weekStartDate.AddDays(i);
+                var weekDayName = weekStartDate.AddDays(i).ToString("ddd");
+                var oracle = GetMayanOracleByDate(weekStartDate.AddDays(i));
+                var tone = GetToneByDate(weekStartDate.AddDays(i));
+                var kin = GetKINByDate(weekStartDate.AddDays(i));
+
+                var card = new CardInfo
+                {
+                    date = dDate,
+                    weekDay = weekDayName,
+                    mainOracle = oracle,
+                    mainToen = tone,
+                    mainKIN = kin
+                };
+
+                cards.Add(card);
+            }
+
+            return cards;
         }
     }
 
@@ -256,11 +444,16 @@ namespace _13MoonDiary.Pages
         }
     }
 
+    /// <summary>
+    /// 每天顯示的資訊卡片內容
+    /// </summary>
     public class CardInfo
     {
         public DateTime date;
         public string weekDay;
-        public int mainoracleEName;
+        public MayanOracle mainOracle;
+        public Tone mainToen;
+        public string mainKIN;
 
     }
 
